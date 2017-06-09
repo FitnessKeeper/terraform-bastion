a1a07e86aa5ee24eeca399e7064f3c3096637290
Steve Huff <steve.huff@runkeeper.com>
2017-06-09 14:57:56 -0400

    add two additional outputs
    * eip
    * fqdn

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "ssh_user" {
 output "security_group_id" {
   value = ["${module.bastion.security_group_id}"]
 }
+
+output "eip" {
+  value = "${aws_eip.bastion.public_ip}"
+}
+
+output "fqdn" {
+  value = "${aws_route53_record.bastion.fqdn}"
+}
