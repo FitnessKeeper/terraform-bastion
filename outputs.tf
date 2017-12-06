1ee06ea61ea65f43695a92d36398a6e1dc7a7250
John Noss <john.noss@runkeeper.com>
2017-12-06 14:18:00 -0500

    Support Terraform 0.11 (#4)
    * outputs have to exist in Terraform 0.11
    
    ...use the splat syntax
    
    * update splat syntax for output

--- outputs.tf
+++ outputs.tf
@@ -11,7 +11,7 @@ output "eip" {
 }
 
 output "fqdn" {
-  value = "${aws_route53_record.bastion.fqdn}"
+  value = "${var.enable_eip ? element(concat(aws_route53_record.bastion.*.fqdn, list("")), 0) : "not created"}"
 }
 
 output "iam_role_arn" {
