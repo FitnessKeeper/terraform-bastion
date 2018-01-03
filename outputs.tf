346e766caa2bb9c5a8be2d1ed2e904eb312ed63e
Tim Hartmann <tfhartmann@gmail.com>
2018-01-03 16:44:16 -0500

    Adding iam_role.name as an output (#7)
    * Adding iam_role.name as an output
    
    * Adding CODEOWNERS file

--- outputs.tf
+++ outputs.tf
@@ -18,6 +18,10 @@ output "iam_role_arn" {
   value = "${aws_iam_role.bastion.arn}"
 }
 
+output "iam_role_name" {
+  value = "${aws_iam_role.bastion.name}"
+}
+
 output "iam_instance_profile_arn" {
   value = "${aws_iam_instance_profile.bastion.arn}"
 }
